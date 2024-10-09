b576b19297657b27260c3edf785d06249b68153a
Felix Schober <felix.schober@rubicon.eu>
2024-10-09 14:38:19 +0200

    DSEAT-22 previous tests now skip Authorization/Authentication

--- RubeesEat/Startup.cs
+++ RubeesEat/Startup.cs
@@ -3,38 +3,46 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using RubeesEat.Model;
 using RubeesEat.Model.DB;
+using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
 
 namespace RubeesEat;
 
 public class Startup
 {
     public IConfiguration Configuration { get; }
+    private readonly IHostingEnvironment _environment;
 
-    public Startup(IConfiguration configuration)
+    public Startup(IConfiguration configuration, IHostingEnvironment environment)
     {
         Configuration = configuration;
+        _environment = environment;
     }
 
     public void ConfigureServices(IServiceCollection services)
     {
-        services.AddAuthentication(options =>
-                {
-                    options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-                    options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
-                })
-                .AddCookie()
-                .AddOpenIdConnect(options =>
-                {
-                    options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-                    options.ClientId = Configuration["OpenId:ClientId"];
-                    options.ClientSecret = Configuration["OpenId:ClientSecret"];
-                    options.Authority = Configuration["OpenId:Authority"];
-                    options.GetClaimsFromUserInfoEndpoint = true;
-                });
-        services.AddAuthorization(option =>
+        if (!_environment.IsEnvironment("Test"))
         {
-            option.FallbackPolicy = option.DefaultPolicy;
-        });
+            services.AddAuthentication(options =>
+                    {
+                        options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                        options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+                    })
+                    .AddCookie()
+                    .AddOpenIdConnect(options =>
+                    {
+                        options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                        options.ClientId = Configuration["OpenId:ClientId"];
+                        options.ClientSecret = Configuration["OpenId:ClientSecret"];
+                        options.Authority = Configuration["OpenId:Authority"];
+                        options.GetClaimsFromUserInfoEndpoint = true;
+                    });
+            services.AddAuthorization(option =>
+            {
+                option.FallbackPolicy = option.DefaultPolicy;
+            });
+        }
+
+    
         services.AddRazorPages();
 
         var connectionString = Configuration["ConnectionStrings:DefaultConnectionString"];
