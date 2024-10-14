fcb571e0d878dc8cddceabffefabc0be52920c6c
irem.arslan <irem.arslan@rubicon.eu>
2024-10-14 12:16:42 +0200

    DSEAT-50 same .js for both SplitBill and EditBill + remove works now for older entries

--- RubeesEat/Controllers/BillController.cs
+++ RubeesEat/Controllers/BillController.cs
@@ -176,6 +176,6 @@ public class BillController(IBillRepository billRepository, IPersonRepository pe
 
         billRepository.Add(bill);
 
-        return TypedResults.Redirect("/SplitBill");
+        return TypedResults.Redirect("/EditBill");
     }
 }
