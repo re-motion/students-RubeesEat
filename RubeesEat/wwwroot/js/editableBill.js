8c2779e4a0a7111340b66e9627329074b05267e5
irem.arslan <irem.arslan@rubicon.eu>
2024-10-15 13:21:59 +0200

    DSEAT-50 added existing bills in json to solve bugs, more new bugs :)

--- RubeesEat/wwwroot/js/editableBill.js
+++ RubeesEat/wwwroot/js/editableBill.js
@@ -1,4 +1,73 @@
 let counter = 0;
+function addExistingEntries(entryLines)
+{
+    for (let i = 1; i < entryLines.length; i++)
+    {
+        const entryLine = entryLines[i];
+        const dropdown = document.getElementById("billPeople");
+        const addPerson = document.getElementById("addPerson");
+        const selectElement = document.getElementById('billPeople');
+        const amount = -entryLine.amount;
+        const selectedPerson =  entryLine.person.firstName + " " + entryLine.person.lastName;
+        const selectedId = entryLine.person.id;
+        const personDiv = document.createElement('div');
+        personDiv.className = 'personDiv';
+
+        const textDiv = document.createElement('div');
+        textDiv.className = 'text';
+        textDiv.textContent = selectedPerson + " ";
+        personDiv.appendChild(textDiv);
+
+        const personIdElement = document.createElement("input");
+        personIdElement.type = "hidden";
+        personIdElement.name = "id" + counter;
+        personIdElement.value = selectedId;
+        document.querySelector("#addedPeople").append(personIdElement);
+
+        const amountElement = document.createElement("input");
+        amountElement.name = "amount" + counter;
+        amountElement.type = "number";
+        amountElement.step = ".01";
+        amountElement.value = amount;
+        if (isTest)
+            amountElement.setAttribute("data-test-id", "input");
+        personDiv.append(amountElement);
+        personDiv.append(document.createElement("br"));
+
+        const button = document.createElement("button");
+        button.innerText = "Remove";
+        button.onclick = function () {
+            personDiv.remove();
+            let addToOptions = document.createElement("option");
+            addToOptions.value = selectedPerson;
+            addToOptions.dataset.id = selectedId;
+            addToOptions.textContent = selectedPerson;
+            selectElement.appendChild(addToOptions);
+
+            addPerson.hidden = false;
+        }
+        if (isTest)
+        {
+            button.setAttribute("data-test-action", "removePeople");
+            button.setAttribute("data-test-click-behavior", "Click");
+        }
+        personDiv.appendChild(button);
+        document.getElementById("addedPeople").appendChild(personDiv);
+
+        counter++;
+
+        dropdown.remove(dropdown.selectedIndex)
+        if (selectElement.options.length === 0) {
+            addPerson.hidden = true;
+        }
+        
+    };
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log(existingEntryLines);
+    addExistingEntries(existingEntryLines);  
+});
 
 function AddPerson() {
     const dropdown = document.getElementById("billPeople");
@@ -6,7 +75,6 @@ function AddPerson() {
     const selectElement = document.getElementById('billPeople');
     const selectedPerson = selectElement.value;
     const selectedId = selectElement.selectedOptions[0].dataset["id"];
-
     const personDiv = document.createElement('div');
     personDiv.className = 'personDiv';
 
